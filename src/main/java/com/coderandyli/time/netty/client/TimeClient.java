e364af31ad62bc7f365dcc96f1e6ec5a0906406d
coderandyli <coderandyli@163.com>
2021-04-12 08:28:48 +0800

    基于netty实现的是时间服务器

---
+++ src/main/java/com/coderandyli/time/netty/client/TimeClient.java
@@ -0,0 +1,54 @@
+package com.coderandyli.time.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.IOException;
+
+/**
+ * @author lizhenzhen
+ * @version 1.0
+ * @date 2021/4/12 上午8:12
+ */
+public class TimeClient {
+    public void connect(int port, String host) throws IOException, InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler(firstMessage))
+                        }
+                    });
+
+            // 发起异步连接操作
+            ChannelFuture f = bootstrap.connect();
+            // 等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        }finally {
+            // 优雅退出
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0){
+            try {
+                port =
+            }catch (NumberFormatException e){
+
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/coderandyli/time/netty/server/TimeServer.java
@@ -0,0 +1,64 @@
+package com.coderandyli.time.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 基于Netty的时间服务器-服务端
+ *
+ * @author lizhenzhen
+ * @version 1.0
+ * @date 2021/4/11 下午9:28
+ */
+public class TimeServer {
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+                // 使用默认值
+            }
+        }
+
+        new TimeServer().bind(port);
+    }
+
+    public void bind(int port) throws Exception {
+        // 配置服务端的NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childHandler(new ChildChannelHandler());
+        // 绑定端口，同步等待成功
+        ChannelFuture f = serverBootstrap.bind(port).sync();
+
+        // 等待服务器监听端口关闭
+        f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    private class  ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+}
